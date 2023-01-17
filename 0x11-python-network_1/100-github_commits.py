c8cfb61f28ee980d1e3969a83cdb1ed8f864e145
Mohammed Kedir <mkdrmash@gmail.com>
2023-01-17 07:21:35 -0800

    0x11-python-network_1

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
-"""
-Takes your Github credentials (username and password) and
-uses the Github API to display your commits
-"""
+""" Github code challenge"""
 import requests
 from sys import argv
 
-
 if __name__ == "__main__":
-    i = 0
-    try:
-        res = requests.get("https://api.github.com/repos/{}/{}/commits".
-                           format(argv[2], argv[1])).json()
-        for commit in res:
-            sha = commit.get("sha")
-            author = commit.get("commit").get("author").get("name")
-            i += 1
-            print("{}: {}".format(sha, author))
-            if i == 10:
-                break
-    except:
-        print("Not a valid PARAMETER")
+    url = "https://api.github.com/repos/{}/{}/commits"\
+          .format(argv[2], argv[1])
+    r = requests.get(url)
+    n = 0
+    for i in r.json():
+        if n < 10:
+            print("{}: {}".format(i.get("sha"),
+                  i.get("commit").get("author").get("name")))
+        n += 1
