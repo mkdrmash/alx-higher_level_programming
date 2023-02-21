774153d4eea1eaf1fa873ec9c2ede4a593065930
Mohammed Kedir <mkdrmash@gmail.com>
2023-02-21 19:04:05 +0300

    0x15-javascript-web_jquery

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,7 @@
+$.getJSON('https://swapi-api.alx-tools.com/api/films/?format=json',
+  function (data) {
+    console.log(data)
+    $.each(data.results, function (index, movie) {
+      $('UL#list_movies').append('<li>' + movie.title + '</li>');
+    });
+  });
